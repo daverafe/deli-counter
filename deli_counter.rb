3557a2d751f2bd542a5b54b53e4b26cdd5ab1c74
David Rafe <david.rafe@gmail.com>
2020-11-16 19:11:46 -0500

    commit tree e9b56c42c118744a3dd7929b06c240ce97b4baa2

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,1 +1,28 @@
-# Write your code here.
+
+def line(katz_deli)
+    numbered_list = []
+    if katz_deli.length != 0
+        katz_deli.each_with_index do |deli, index|
+            numbered_list << "#{index + 1}. #{deli}"
+        end 
+        puts "The line is currently: #{numbered_list.join(" ")}"
+    else
+        puts "The line is currently empty."
+    end
+end
+
+def take_a_number(katz_deli, person)
+    katz_deli << person 
+        puts "Welcome, #{person}. You are number #{katz_deli.length} in line."
+end
+
+def now_serving(katz_deli)
+    if katz_deli.length == 0
+        puts "There is nobody waiting to be served!"
+    else
+            puts "Currently serving #{katz_deli[0]}."
+            katz_deli.shift
+    end
+end
+
+line(["dave", "aysan"])
